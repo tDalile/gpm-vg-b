776fbe6a33b34555ae3264ec0c422b3b05ee58ec
dalile <didadude.ii@gmail.com>
2021-01-09 14:10:17 +0100

    implemted calculation of age

--- src/main/java/de/thkoeln/inf/gpm/vgb/delegate/DummyScriptTaskDelegate.java
+++ src/main/java/de/thkoeln/inf/gpm/vgb/delegate/DummyScriptTaskDelegate.java
@@ -5,7 +5,10 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,19 +17,33 @@ public class DummyScriptTaskDelegate implements JavaDelegate {
     public void execute(DelegateExecution execution) throws Exception {
         Map<String, Object> processVariables = new HashMap();
 
+        Date geburtstag = (Date) execution.getVariable("mgeburtsdatum");
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        Integer alter = Period.between(LocalDate.now(), (LocalDate) ((execution.getVariable("mgeburtsdatum")))).getYears();
+
+
+        Double bmi = (Double)execution.getVariable("mgewicht") / Math.sqrt((Double) execution.getVariable("mgroesse"));
 
         /**
         * TODO: Compute each variable
         * Integer bmi
         **/
 
-        processVariables.put("alter", alter);
+        processVariables.put("alter", calcAge(geburtstag));
+        processVariables.put("bmi", bmi);
 
 
         execution.setVariables(processVariables);
 
+    }
+
+    private Integer calcAge(Date geburtstag) {
+        //java.util.Date --> java.time.LocalDateTime
+        LocalDateTime gLocDT = geburtstag.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        //java.time.LocalDateTime --> LocalDate
+        LocalDate gLocD = gLocDT.toLocalDate();
+
+        int alter;
+        return alter = Period.between(LocalDate.now(), gLocD).getYears();
     }
 }
