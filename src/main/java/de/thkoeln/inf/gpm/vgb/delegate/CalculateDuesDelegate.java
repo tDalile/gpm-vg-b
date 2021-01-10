e3ae179ad5413ea3f20583d15da3ace04f1e8f45
KayRuck <kay.ruck@yahoo.de>
2021-01-10 21:14:44 +0100

    implemented 'chechout' workflow. Warning: not ready for merge

--- src/main/java/de/thkoeln/inf/gpm/vgb/delegate/CalculateDuesDelegate.java
+++ src/main/java/de/thkoeln/inf/gpm/vgb/delegate/CalculateDuesDelegate.java
@@ -1,4 +1,50 @@
 package de.thkoeln.inf.gpm.vgb.delegate;
 
-public class CalculateDuesDelegate {
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculateDuesDelegate implements JavaDelegate {
+
+//    private boolean isPremium;
+    private double riskDue = 0.0;
+    private int age;
+
+//    CalculateDuesDelegate(int age){
+//        this.age = age;
+//    }
+//
+//    public CalculateDuesDelegate(int age, double riskDue){
+//        this.age = age;
+//        this.riskDue = riskDue;
+//    }
+
+    double calculateDue(){
+
+        double initialDue = 110.0;
+        double multiplier = 10.0;
+
+
+        double monthlyDue = multiplier * age + riskDue;
+
+        if (monthlyDue > initialDue) monthlyDue = initialDue + riskDue;
+
+        return monthlyDue;
+    }
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        Map<String, Object> processVariables;
+        processVariables = delegateExecution.getVariables();
+
+        // TODO: change names of the variables
+        age = (int) processVariables.get("age");
+        riskDue = (double) processVariables.get("riskDue");
+
+        processVariables.put("monthlyDue", calculateDue());
+
+        delegateExecution.setVariables(processVariables);
+    }
 }
---
+++ src/test/java/de/thkoeln/inf/gpm/vgb/delegate/CalculateDuesDelegateTest.java
@@ -0,0 +1,32 @@
+package de.thkoeln.inf.gpm.vgb.delegate;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CalculateDuesDelegateTest {
+
+
+    // Funktioniert!
+//    @Test
+//    public void testCalculateAge()
+//    {
+//        CalculateDuesDelegate calculate = new CalculateDuesDelegate(20);
+//        assertEquals(110.0, calculate.calculateDue(), 0.001);
+//    }
+//
+//    @Test
+//    public void testCalculateAgeAndRiskDue()
+//    {
+//        CalculateDuesDelegate calculate = new CalculateDuesDelegate(2, 12.0);
+//        assertEquals(32.0, calculate.calculateDue(), 0.001);
+//    }
+//
+//    @Test
+//    public void testCalculateAgeAndRiskDue2()
+//    {
+//        CalculateDuesDelegate calculate = new CalculateDuesDelegate(20, 12.0);
+//        assertEquals(122.0, calculate.calculateDue(), 0.001);
+//    }
+
+}
