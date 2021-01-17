6eca70f0f79f97bc03f0d26cd330fe47dde7ad9a
KayRuck <kay.nicole.ruck@web.de>
2021-01-17 14:07:08 +0100

    added form check-policy als HTMl & in BPMN

--- src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/ContinueProcessByObjectionMessageDelegate.java
+++ src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/ContinueProcessByObjectionMessageDelegate.java
@@ -8,16 +8,8 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * This is an easy adapter implementation 
- * illustrating how a Java Delegate can be used 
- * from within a BPMN 2.0 Send Task to reply to another process
- */
 public class ContinueProcessByObjectionMessageDelegate implements JavaDelegate {
 
-	 /**
-	 *
-	 */
 	public void execute(DelegateExecution execution) {
 		RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
 		
@@ -27,30 +19,16 @@ public class ContinueProcessByObjectionMessageDelegate implements JavaDelegate {
 		// put, if not by user task
 		processVariables.putIfAbsent("approved", true);			
 
+		// ProcessInstance startProcessInstanceByMessage(String messageName, Map<String, Object> processVariables);
+
 		// set the correlation id to identify the waiting process
 		String correlationId = (String) processVariables.get("correlationId");
+
 
 		runtimeService
 				.createMessageCorrelation("objectionMessage")
 				.setVariables(processVariables)
-				// set the correlation id as processInstanceBusinessKey of the waiting process
 				.processInstanceBusinessKey(correlationId)
 				.correlate();
-
-		// set the correlation id to identify the waiting process
-
-
-		/*
-		processVariables.put("kundennr", execution.getVariable("mkundennr"));
-		processVariables.put("eintritt", execution.getVariable("meintritt"));
-		processVariables.put("versicherter_id", execution.getVariable("mversicherter_id"));
-		processVariables.put("nachname", execution.getVariable("mnachname"));
-		processVariables.put("vorname", execution.getVariable("mvorname"));
-		processVariables.put("geburtsdatum", execution.getVariable("mgeburtsdatum"));
-		processVariables.put("geschlecht", execution.getVariable("mgeschlecht"));
-		processVariables.put("groesse", execution.getVariable("mgroesse"));
-		processVariables.put("gewicht", execution.getVariable("mgewicht"));
-
-		 */
 	  }
 }
