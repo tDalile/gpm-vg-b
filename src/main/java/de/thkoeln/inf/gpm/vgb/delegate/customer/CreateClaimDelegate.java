1f9fc0ef1d76fa2a3fc39920d6151076992f9ebf
pschm <philipp.schmeier@gmail.com>
2021-01-19 19:50:24 +0100

    fix birthdate source

--- src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/CreateClaimDelegate.java
+++ src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/CreateClaimDelegate.java
@@ -1,22 +1,16 @@
 package de.thkoeln.inf.gpm.vgb.delegate.customer;
 
 import de.thkoeln.inf.gpm.vgb.model.ProcessContext;
+import de.thkoeln.inf.gpm.vgb.model.external.Insurant;
+import de.thkoeln.inf.gpm.vgb.util.DateUtil;
+import lombok.val;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.Map;
 
 public class CreateClaimDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        Map<String, Object> processVariables;
-        processVariables = delegateExecution.getVariables();
         ProcessContext processContext = new ProcessContext(delegateExecution);
 
         saveClaim(
@@ -26,23 +20,14 @@ public class CreateClaimDelegate implements JavaDelegate {
                 processContext.getInternal().getClaimBMI()
         );
 
-        processContext.getInternal().setInsurantAge(calcAge(processContext.getInternal().getInsurantBirthdate()));
+        val insurant = Insurant.findById(processContext.getInternal().getInsurantId());
+        val age = DateUtil.calcAge(insurant.getBirthdate());
 
+        processContext.getInternal().setInsurantAge(age);
     }
 
     private void saveClaim(long customerId, long insurantId, boolean isPremiumApplication, double bmi) {
         // TODO save claim in db
         // TODO set claimId processVariable
-    }
-
-    private Integer calcAge(Date geburtstag) {
-        //java.util.Date --> java.time.LocalDateTime
-        LocalDateTime gLocDT = geburtstag.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-
-        //java.time.LocalDateTime --> LocalDate
-        LocalDate gLocD = gLocDT.toLocalDate();
-
-        int alter;
-        return alter = Period.between(gLocD, LocalDate.now()).getYears();
     }
 }
