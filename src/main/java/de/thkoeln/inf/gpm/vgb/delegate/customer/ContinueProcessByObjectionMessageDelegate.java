d71918b46d52a406d7e9d79b85bdf0313e6fff20
pschm <philipp.schmeier@gmail.com>
2021-01-25 14:55:24 +0100

    add empty diseas if none is selected

--- src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/ContinueProcessByObjectionMessageDelegate.java
+++ src/main/java/de/thkoeln/inf/gpm/vgb/delegate/customer/ContinueProcessByObjectionMessageDelegate.java
@@ -1,36 +1,36 @@
 package de.thkoeln.inf.gpm.vgb.delegate.customer;
 
 
-import de.thkoeln.inf.gpm.vgb.model.ProcessContext;
+import de.thkoeln.inf.gpm.vgb.model.ProcessVariableConstants;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ContinueProcessByObjectionMessageDelegate implements JavaDelegate {
 
-	public void execute(DelegateExecution execution) {
-		RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
-		ProcessContext processContext = new ProcessContext(execution);
-		
-		// fill the reply message with this instance process variables
-		Map<String, Object> processVariables = new HashMap();
-		processVariables = execution.getVariables();
-		// put, if not by user task
-		processVariables.putIfAbsent("approved", true);			
+    public void execute(DelegateExecution execution) {
+        Map<String, Object> processVariables = execution.getVariables();
 
-		// set the correlation id to identify the waiting process
-		// TODO: add to Variable Store
-		String correlationId = (String) processVariables.get("correlationId");
+        processVariables.putIfAbsent("approved", true);
 
-		runtimeService
-				.createMessageCorrelation("processContext.getInternal().getObjectionMessage()")
-				.setVariables(processVariables)
-				// set the correlation id as processInstanceBusinessKey of the waiting process
-				.processInstanceBusinessKey(correlationId)
-				.correlate();
+        // set the correlation id to identify the waiting process
+        // TODO: add to Variable Store
+        String correlationId = (String) processVariables.get("correlationId");
+        System.out.println("correlationID: " + correlationId);
 
-	  }
+        RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+//		runtimeService
+//				.createMessageCorrelation(ProcessVariableConstants.INT_OBJECTION_MESSAGE)
+//				.setVariables(processVariables)
+//				// set the correlation id as processInstanceBusinessKey of the waiting process
+//				.processInstanceBusinessKey(correlationId)
+//				.correlate();
+
+        runtimeService.startProcessInstanceByMessage(
+                ProcessVariableConstants.INT_OBJECTION_MESSAGE,
+                processVariables
+        );
+    }
 }
